986c8153c373361cf726b2132e988f89bcede77d
Sajal Halder <49409759+sajal48@users.noreply.github.com>
2023-02-03 10:17:47 +0600

    Delete product-service directory

--- product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productservice.repository;
-
-import com.example.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product,String> {
-}
